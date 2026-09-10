7d6d0e0629d3bdba33688897b564564f98aa5602
Pedro Passos <pedropassosforwork@gmail.com>
2026-09-10 15:58:09 -0300

    v6

--- src/main/java/Exercicios/Q10.java
+++ src/main/java/Exercicios/Q10.java
@@ -6,15 +6,13 @@ public class Q10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // 2. Ler salário
         System.out.print("Informe o salário do cliente: R$ ");
         double salario = scanner.nextDouble();
 
-        // 3. Ler valor da parcela
+
         System.out.print("Informe o valor da parcela desejada: R$ ");
         double parcela = scanner.nextDouble();
 
-        // 4. Verificar se parcela <= 30% do salário
         if (parcela <= (salario * 0.30)) {
             // 5. Exibir resultado
             System.out.println("Empréstimo APROVADO.");
